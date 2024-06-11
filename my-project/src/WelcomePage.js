8f25794120338f533826364afe3c8e5e18ff0d9a
collins-ma <collinsmboya45@gmail.com>
2024-06-11 13:26:56 +0300

    modified a file

--- my-project/src/WelcomePage.js
+++ my-project/src/WelcomePage.js
@@ -7,18 +7,11 @@ function WelcomePage() {
       <div className="flex-grow"></div>
       <div className="w-full text-center text-white mb-12">
         <h1 className="text-4xl font-extrabold">
-          Welcome to <span className="nowrap">Collins Kemboi. Repairs!</span>
+          Welcome to the User and Notes Management System
         </h1>
         <p className="mt-2 text-lg">
-          Located in Beautiful Ruiru Town near Zetech University, Collins Kemboi. Repairs provides a trained staff ready to meet your tech repair needs.
+          Your all-in-one platform for managing users and notes efficiently. This application is designed to provide a seamless and intuitive experience for administrators, managers, and employees.
         </p>
-        <address className="not-italic mt-4">
-          Collins Kemboi. Repairs<br />
-          555 Ruiru Drive<br />
-          Nairobi City,12345<br />
-          <a href="tel:+0743951762" className="underline">(555) 555-5555</a>
-        </address>
-        <p className="mt-2">Owner: Collins Kemboi</p>
       </div>
       <div className="w-full">
         <hr className="border-t border-white" />
